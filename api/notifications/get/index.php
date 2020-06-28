ff28390301f97042370291d14797c0f030a4c344
Pablo Varela <pablops3@gmail.com>
2020-06-28 13:19:57 -0300

    Intento 002

--- api/notifications/get/index.php
+++ api/notifications/get/index.php
@@ -11,13 +11,13 @@ global $access_token,$collector_id,$notificationJSON;
 
 $lastResource = file_get_contents('../notifications.txt');
 
-$url= "";
+$url= $lastResource;
 
-// REVISAR AQUÍ:
+// REVISA AQUÍ: Listo
 // Agrega la $url necesaria para revisar el estado del pago en base al recurso recibido de la notificación
 
 // Sustituye el método por su correspondiente: get, put, post, delete
 
-curl_call("delete",."$url","");
+curl_call("GET","$url","");
 
  ?>
